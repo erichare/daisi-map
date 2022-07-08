57edd98e5333fe0e8a8a6e022aa8114fee6567e6
Eric Hare <ericrhare@gmail.com>
2022-07-08 15:46:49 -0700

    Update entry.py

--- entry.py
+++ entry.py
@@ -2,8 +2,9 @@ import time
 from pydaisi import Daisi
 
 def test_func():
-    with Daisi("Add Two Numbers") as my_daisi:
+    with Daisi("Add Two Numbers", instance="dev3") as my_daisi:
         dbe = my_daisi.map(func="subtract", args_list=[{"firstNumber": 5, "secondNumber": x} for x in range(10)])
+        dbe.start()
         time.sleep(5)
         print(dbe.value)
 
