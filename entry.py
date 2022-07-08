a3cbc6f8f89135b58beefcfcf6f05b45b8d08b47
Eric Hare <ericrhare@gmail.com>
2022-07-08 15:43:56 -0700

    Update entry.py

--- entry.py
+++ entry.py
@@ -3,6 +3,7 @@ from pydaisi import Daisi
 def test_func():
     with Daisi("Add Two Numbers") as my_daisi:
         dbe = my_daisi.map(func="subtract", args_list=[{"firstNumber": 5, "secondNumber": x} for x in range(10)])
+        time.sleep(5)
         print(dbe.value)
 
     return("Completed")
